4a970dca3534e9ab166bcdc576f1638010b43b82
Sinus44 <59234792+Sinus44@users.noreply.github.com>
2020-12-10 11:25:25 +0300

    Update turtle.lua

--- turtle.lua
+++ turtle.lua
@@ -1,4 +1,20 @@
 shell.run("clear")
+print("Enter channel: ")
+ch = io.read()
+ch=tonumber(ch)
 
 modem = peripheral.find("modem")
 modem.open(ch)
+
+while true do
+  _,_,ch,rech,m,_=os.pullEvent("modem_message")
+  if m=="w" then
+    turtle.forward()
+  elseif m=="s"
+    turtle.back()
+  elseif m=="d" then
+    turtle.turnRight()
+  elseif m=="a" then
+    turtle.turnLeft()
+  end
+end
